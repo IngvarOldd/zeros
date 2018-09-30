6ac867692d05f0a950d81c3663539a4ab0e1f758
IngvarOldd <lyushinskij.i.a@gmail.com>
2018-09-30 18:51:37 +0300

    7th task 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let answer = 0;
+  while(number = Math.floor(number/5)) {
+    answer += number;
+  }
+  return answer;
 }
